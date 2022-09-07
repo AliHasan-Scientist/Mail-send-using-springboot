fd0d883772ceac7468aa231d45c133a3087ebe71
Roy Ali Hasan <royalihasan0987@gmail.com>
2022-09-07 12:05:44 +0500

    Add Mail Service For File Attachments

--- src/main/java/Mailsendusingspringboot/controller/MailController.java
+++ src/main/java/Mailsendusingspringboot/controller/MailController.java
@@ -5,16 +5,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.mail.MessagingException;
+
 @RestController
 public class MailController {
     public String fromMail = "informativepath0987@gmail.com";
     @Autowired
     private SendMailService mailService;
+    public static final String path = "C:\\Users\\Roy Ali Hasan\\Documents\\Java Projects\\RESTful-Web-APIs-using-MariaDB\\src\\main\\java\\RESTfulWebAPIsusingMariaDB\\assets\\roy.jpg";
 
     @GetMapping("/mail")
     public String testMail() {
 
         mailService.sendSimpleMail(fromMail, "royalihasan0987@gmail.com", "Hello Our Supporter", "Welcome");
+
+        return "Mail send Successfully";
+    }
+
+    @GetMapping("/mail/attachment")
+    public String attachment() throws MessagingException {
+
+        mailService.sendMailWithAttachments(fromMail, "royalihasan0987@gmail.com", "Hello Our Supporter", "Welcome", path);
+
         return "Mail send Successfully";
     }
 }
--- src/main/java/Mailsendusingspringboot/mail/SendMailService.java
+++ src/main/java/Mailsendusingspringboot/mail/SendMailService.java
@@ -1,14 +1,21 @@
 package Mailsendusingspringboot.mail;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.MailSender;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
 
 @Service
 public class SendMailService {
     @Autowired
-    private MailSender mailSender;
+    private JavaMailSender mailSender;
 
     public void sendSimpleMail(String fromMail, String toMail, String body, String subject) {
 
@@ -18,5 +25,21 @@ public class SendMailService {
         message.setText(body);
         message.setSubject(subject);
         mailSender.send(message);
+
     }
+
+    public void sendMailWithAttachments(String fromMail, String toMail, String body, String subject, String attachment) throws MessagingException {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setFrom("informativepath0987@gmail.com");
+        mimeMessageHelper.setTo(toMail);
+        mimeMessageHelper.setSubject(subject);
+        mimeMessageHelper.setText(body);
+        FileSystemResource fileSystemResource = new FileSystemResource(new File(attachment));
+        mimeMessageHelper.addAttachment(fileSystemResource.getFilename(), fileSystemResource);
+        mailSender.send(mimeMessage);
+        System.out.println("mail send successfully with attachment");
+
+    }
+
 }
